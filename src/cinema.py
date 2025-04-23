2b5779601a36365175fa20fb2d2613b8c1a35c5a
MaeinesenieaM <david.costa.gomes71@gmail.com>
2025-04-22 23:20:02 -0300

    Comentários, comentários e mais comentários!

--- src/cinema.py
+++ src/cinema.py
@@ -1,6 +1,8 @@
 import threading
 import time
 
+#Objeto Principal do servidor; ele possui uma lista de tarefas requisitadas, que são compridas
+# assim que pedidas.
 class Servidor:
     def __init__(self, quantidade_assentos: int):
         self.requests = {}
@@ -10,24 +12,28 @@ class Servidor:
         threading.Thread(target=self.run, daemon=True).start()
         print("[SERVER] Servidor iniciado!")
 
-    def get_client(self, nome: str) -> "Cliente":
-        return self.clients[nome]
-
+    #Função e loop principal do Servidor, é aki onde ele fica checando suas tarefas infinitamente
+    # até que a thread principal termine.
     def run(self):
         while True:
             with self.lock:
                 #Sim, o servidor é apenas isso.
                 self.check_requests()
 
+    #Essa função existe apenas na facilitação de pedir um Cliente e complementar o IDE.
+    def get_client(self, nome: str) -> "Cliente":
+        return self.clients[nome]
+
     #Aki é a lógica inteira para checar os comandos enviados pelos clientes
-    #a cada comando completo, o comando e retirado da lista self.requests
-    #para evitar repetição.
+    # a cada comando completo, o comando e retirado da lista self.requests
+    # para evitar repetição.
     def check_requests(self):
         remove_queue = []
         current_requests = self.requests.copy()
         for tipo, valores in current_requests.items():
             for valor in valores:
                 match tipo:
+                    #Tarefa para reservar um assento ao nome de um cliente.
                     case "reservar":
                         nome, assento = valor
                         if assento in self.assentos:
@@ -42,16 +48,21 @@ class Servidor:
                             print(f"[SERVER] Assento {assento} inválido.")
                             self.get_client(nome).add_response("reservado", None)
                         remove_queue.append((tipo, valor))
+                    #Tarefa para devolver a quantidade de assentos no sistema par o cliente
+                    # e seu estados.
                     case "listar":
                         nome = valor
                         self.get_client(nome).add_response("assentos", self.assentos)
                         print(f"[SERVER] Enviado assentos para: {nome}")
                         remove_queue.append((tipo, valor))
+                    #Esta tarefa serve mais como um aviso do cliente que ele esta sendo desligado;
+                    # assim o servidor pode retirar o cliente na sua lista de conexões para evitar problemas.
                     case "sair":
                         nome = valor
                         self.clients.pop(nome, None)
                         print(f"[SERVER] Cliente {nome} saiu.")
                         remove_queue.append((tipo, nome))
+                    #Caso o servidor receba uma tarefa não registrada.
                     case _:
                         print(f"[SERVER] Recebido pedido invalido!: {tipo}")
                         remove_queue.append((tipo, valor))
@@ -61,16 +72,22 @@ class Servidor:
             if tipo in self.requests and valor in self.requests[tipo]:
                 self.requests[tipo].remove(valor)
 
+    #Adiciona uma tarefa na lista do servidor, onde fica na espera de ser concluída e descartada.
     def add_request(self, request_type, param):
         if request_type not in self.requests:
             self.requests[request_type] = []
         self.requests[request_type].append(param)
 
+    #Adiciona um cliente para o servidor, sem isso o servidor não consegue se comunicar com tais
+    # clientes de fora.
     def add_client(self, name: str, cliente: "Cliente"):
         if name in self.clients:
             print("TODO!")
         self.clients[name] = cliente
 
+#Cliente é a parte visual do usuário, quando criado o cliente ele começa em um estado de suspensão,
+# e para acordá-lo é necessário chamar a função "start()" para que toma controle do CMD.
+#Isso serve que múltiplos clientes não se conflitam com os outros quando for usa-los pelo prompt de comando.
 class Cliente:
     def __init__(self, nome: str, servidor: "Servidor"):
         self.server = servidor
@@ -79,12 +96,25 @@ class Cliente:
         servidor.add_client(self.nome, self) #Adiciona o cliente para a lista do servidor.
         print(f"[Cliente: {self.nome}] Criado!")
 
+    #Acorda o cliente, e toma posse do prompt de comando, como o input().
     def start(self):
-        print(f"[Cliente: {self.nome}] Iniciado!")
+        print(f"[Cliente: {self.nome}] Acordado!")
         thread = threading.Thread(target=self._menu)
         thread.start()
         return thread
 
+    #Esta é uma função usada principalmente pelo servidor, como forma de comunicação.
+    #
+    #Diferente do servidor, os clientes podem escutar apenas um tipo de resposta por vez,
+    # já que ao pedir por uma tarefa, eles esperam uma resposta do servidor com o valor esperado,
+    # caso o valor não for como esperado o cliente pode desenvolver o erro e notificar o usuário.
+    #
+    #Não é recomendado chamá-la fora do contexto de respostas entre o servidor e cliente!
+    #Exceto se você querer quebrar o cliente!
+    def add_response(self, tipo: str, valor):
+        self.response[tipo] = valor
+
+    #A Interface do usuário.
     def _menu(self):
         while True:
             print("\nOpções:")
@@ -106,6 +136,8 @@ class Cliente:
                 print("Opção inválida.")
                 time.sleep(0.5)
 
+    #Tenta reservar um assento no servidor, após a lógica ser tratada pelo servidor, a resposta
+    # é recebida para ser mostrada ao usuário.
     def _reservar(self, assento):
         self.server.add_request("reservar", (self.nome, assento))
         resposta = self._listen_type("reservado")
@@ -114,6 +146,7 @@ class Cliente:
         else:
             print(resposta)
 
+    #Pede a quantidade de assentos do servidor, e assim que recebida é mostrada na tela.
     def _listar_assentos(self):
         self.server.add_request("listar", self.nome)
         assentos = self._listen_type("assentos")
@@ -122,9 +155,12 @@ class Cliente:
         else:
             self._imprimir_assentos(assentos)
 
-    def add_response(self, tipo: str, valor):
-        self.response[tipo] = valor
-
+    #Aki esta a lógica de mensagens esperadas, é chamada assim que uma tarefa é requisitada.
+    #
+    #Após o envio da tarefa, o cliente fica esperando por um tipo de resposta do servidor,
+    # caso o servidor demore demais, ira ignorar o servidor e receber None.
+    #
+    #!AVISO! Talvez quebre caso o servidor mandar o tipo errado de mensagem!
     def _listen_type(self, tipo: str):
         espera = time.time()
         while True:
@@ -137,6 +173,7 @@ class Cliente:
             self.response.pop(tipo, None)
             return resposta
 
+    #Autoexplicativo
     def _imprimir_assentos(self, assentos):
         print(f"\nAssentos disponíveis para {self.nome}:\n")
         print("-" * 22, "TELA", "-" * 22)
--- src/main.py
+++ src/main.py
@@ -3,15 +3,20 @@ import time
 
 if __name__ == "__main__":
     assentos = input("quantos assentos?")
+    #O servidor é iniciado assim que é criado, com sua propria thread e loop.
     servidor = cinema.Servidor(int(assentos))
 
     while True:
         nome = str(input("Digite seu nome: "))
+        #Diferente do servidor, é necessário acordar o cliente para usar pelo prompt de comando.
         cliente = cinema.Cliente(nome, servidor)
+        #Acorda o cliente e recebe sua Thread. (Fortemente recomendado acordar um cliente por vez.)
         cliente_thread = cliente.start()
 
-        cliente_thread.join()
+        cliente_thread.join() #Espera que o cliente termine sua função.
 
+        #Aki abaixo é mais exclusivo para esta demonstração, é possível criar múltiplos clientes e
+        # usa-los um por vez. Não é o melhor sistema de todos, mas foi o que encontramos.
         continuar = input("Deseja cadastrar outro usuário? (s/n): ").strip().lower()
         if continuar != 's':
             print("Encerrando sistema.")
